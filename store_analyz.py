7bbb055498f513e1929d596da93f4445404ba6c1
rakshith077-bn <73026460+rakshith077-bn@users.noreply.github.com>
2023-11-08 04:27:13 +0530

    Nov7th

--- store_analyz.py
+++ store_analyz.py
@@ -34,18 +34,18 @@ except Exception as e:
     
     
 # Since it's working, I'll print it for the whole dataset and then move on to writing those into different values
-try:
-    with open(file_name, 'r', newline='') as file:
-        reader = csv.reader(file)
-        next(reader) #Skipping the first heading line in the dataset
-        row_count = 0
-        for row in reader:
-            row_tuple = ("INSERT",) + tuple(row_tuple)
-            print(row_tuple)
 
-except Exception as e:
-    print(f"An error occurred: {e}")
 
+
+# To make sure the dataset has not stored the value "INSERT", I'll print it the first 50 columns of it
+with open(file_name, 'r') as file:
+    for i in range(50):
+        line = file.readline()
+        if line:
+            print(line)
+        else:
+            break
+# Hence we can see that the value "INSERT" is not stored in the dataset
 
 
 # Creating 3 directories
@@ -57,17 +57,41 @@ for dir_name in dir_names:
     try:
         os.mkdir(dir_name)
         print(f"Directory {dir_name} created successfully")
+
     except FileExistsError:
         print(f"Directory {dir_name} already exists")
+   
     except Exception as e:
-        print("An Error occured")
+        print(f"An Error occured: {e}")
 
-for dir_name in dir_names:
-    file_path = os.path.join(dir_name, 'example.txt')
+
+#Creating files respective to the above directories    
+file_names = ["Page 0", "Page 1", "Page 2"]
+
+
+
+for name in file_names:
+    try:
+        for name in file_names:
+            path_file = os.path.join(dir_names, name)
+        if not os.path.exists(dir_names):
+            os.mkdir(dir_names)
+        print("File Created successfully")
+    
+    except FileExistsError:
+        print(f"File {name} already exists")
+    
+    except FileNotFoundError:
+        print(f"Directory {dir_names} not found")
+    
+    except Exception as e:
+        print(f"An error occured: {e}")
+    
+    
     
     try:
         with open(file_path, 'w') as file:
-            file.write(f"This is an example file in {dir_name}")
+            file.write(f"This is a file in {dir_name}")
         print(f"File created successfully in {dir_name}")
     except Exception as e:
         print(f"An error occured: {e}")
